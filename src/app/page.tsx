05ff085ee7be774166ab2e6a1372d59629a563d0
FabianoLaureano <fabianoandrelaureano@gmail.com>
2025-08-06 15:46:46 -0300

    feat: add category selector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { desc } from "drizzle-orm";
 import Image from "next/image";
 
-//import CategorySelector from "@/components/common/category-selector";
+import CategorySelector from "@/components/common/category-selector";
 //import Footer from "@/components/common/footer";
 import { Header } from "@/components/common/header";
 import ProductList from "@/components/common/product-list";
@@ -20,7 +20,7 @@ const Home = async () => {
       variants: true,
     },
   });
-  //const categories = await db.query.categoryTable.findMany({});
+  const categories = await db.query.categoryTable.findMany({});
 
   return (
     <>
@@ -39,9 +39,9 @@ const Home = async () => {
 
         <ProductList products={products} title="Mais vendidos" />
 
-        {/* <div className="px-5">
+        <div className="px-5">
           <CategorySelector categories={categories} />
-        </div> */}
+        </div>
 
         <div className="px-5">
           <Image
